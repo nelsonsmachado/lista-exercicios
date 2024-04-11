437ac107d927f1c104a5fcfcfa6776212ea96989
Nelson Machado <nelson.machado@dbserver.com.br>
2024-04-11 14:25:38 -0300

    modificando modo impressão e corrigindo commit: exercicio 012 adicionado ao repositorio

--- src/JuntaVetores.java
+++ src/JuntaVetores.java
@@ -16,24 +16,21 @@ public class JuntaVetores {
         System.arraycopy(vetor1, 0, novoVetor, 0, vetor1.length);
         System.arraycopy(vetor2, 0, novoVetor, vetor1.length, vetor2.length);
 
-
         Arrays.sort(novoVetor);
 
-
-        System.out.println("Vetor 1:");
+        System.out.print("Vetor1:n ");
         exibirVetor(vetor1);
-        System.out.println("\nVetor 2:");
+        System.out.print("Vetor2: ");
         exibirVetor(vetor2);
-        System.out.println("\nNovo Vetor:");
+        System.out.print("Novo Vetor: n");
         exibirVetor(novoVetor);
     }
-
 
     public static void exibirVetor(int[] vetor) {
         for (int j : vetor) {
             System.out.print(j + " ");
         }
-        System.out.println(); // Adicionar uma nova linha após exibir o vetor
+        System.out.println();
     }
 
     // Método para gerar um vetor de tamanho especificado com valores inteiros aleatórios
@@ -41,7 +38,7 @@ public class JuntaVetores {
         int[] vetor = new int[tamanho];
         Random random = new Random();
         for (int i = 0; i < tamanho; i++) {
-            vetor[i] = random.nextInt(100); // Intervalo de valores aleatórios de 0 a 99
+            vetor[i] = random.nextInt(100);
         }
         return vetor;
     }
